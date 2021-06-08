b186754fbcb8a3e68def4fada3477e948cd76a5f
hoatp <hoatruongdev09@gmail.com>
2021-06-08 14:35:11 +0700

    go to month transction

--- client/src/Components/AppPage/TransactionsPage/YearlyTransactionPage.js
+++ client/src/Components/AppPage/TransactionsPage/YearlyTransactionPage.js
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import formatMoney from '../../../Utils/formatMoney'
+import { v4 as uuidv4 } from 'uuid';
 
 const YearlyTransactionPage = ({ active, transactions, selectDate }) => {
     const monthNames = ["January", "February", "March", "April", "May", "June",
@@ -6,10 +8,18 @@ const YearlyTransactionPage = ({ active, transactions, selectDate }) => {
     ];
 
     const CreateYearCell = ({ month, income, expense }) => {
+        const date = new Date(new Date(selectDate).getFullYear(), month, 1)
+        const linkTarget = {
+            pathname: `/dashboard/transactions?tab=monthly&time=${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`,
+            key: uuidv4(),
+            state: {
+                applied: true
+            }
+        }
         return (
             <tr>
                 <td className="text-bold-500 year-cell">
-                    <a role="button" className="btn btn-outline-primary">{month}</a>
+                    <Link to={linkTarget} className="btn btn-outline-primary">{monthNames[month]}</Link>
                 </td>
                 <td className="text-end year-cell text-info" >{income}</td>
                 <td className="text-end year-cell text-danger" >{expense}</td>
@@ -28,7 +38,7 @@ const YearlyTransactionPage = ({ active, transactions, selectDate }) => {
         transaction.filter(record => record.expense_type_id == '2').forEach(tran => totalExpense += parseInt(tran.money_amount))
         transaction.filter(record => record.expense_type_id == '1').forEach(tran => totalIncome += parseInt(tran.money_amount))
         return (
-            <CreateYearCell month={monthNames[month]} income={formatMoney(totalIncome)} expense={formatMoney(totalExpense)} />
+            <CreateYearCell month={month} income={formatMoney(totalIncome)} expense={formatMoney(totalExpense)} />
         )
     }
     return (
